c526e092634d7bb9fc38a6e1aa33d17568cca081
Andrés Felipe Babativa Betancourt <andresfelipe_1611@outlook.com>
2022-03-26 17:28:23 -0500

    Modificando base de datos

--- Vistas/Forms/register.php
+++ Vistas/Forms/register.php
@@ -14,24 +14,24 @@
         include '../navbar.php'; 
     ?>
     <div class="contain">
-        <form action="Procesos/procesoRegistro.php" method="post">
+        <form action="Procesos/procesoRegistro.php" id="formulario" method="post">
             <label class="title">Registrarse | </label>
 
             <a href="login.php" class="link">Ya tengo una cuenta</a><br><br>
 
             <label for="">Nombre</label><br>
-            <input type="text" name="nombre" placeholder="Ingrese su nombre completo"><br>
+            <input type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre completo"><br>
             <label for="">Apellidos</label><br>
-            <input type="text" name="apellidos" placeholder="Ingrese sus apellidos"><br>
+            <input type="text" name="apellidos" id="apellidos" placeholder="Ingrese sus apellidos"><br>
             <label for="">Tipo de documento</label><br>
-            <select name="tipo_docu">
+            <select name="tipo_docu" id="tipo_docu">
                 <option value="">--Seleccionar--</option>
                 <option value="Tarjeta de Identidad.">Tarjeta de identidad</option>
                 <option value="Cédula de Ciudadanía.">Cédula de ciudadanía</option>
                 <option value="Cédula Extranjera">Cédula extranjera</option>
             </select><br>
             <label for="">Número de documento</label><br>
-            <input type="text" name="num_docu" placeholder="Ingrese su número de documento"><br>
+            <input type="text" name="num_docu" id="num_docu" placeholder="Ingrese su número de documento"><br>
             <label for="">Rol</label><br>
             <select name="roles">
                 <option value="">--Seleccionar--</option>
@@ -39,12 +39,15 @@
                 <option value="2">Aprendiz</option>                
             </select><br>
             <label for="">Correo electrónico</label><br>
-            <input type="email" name="correo" placeholder="Ingrese su correo electrónico"><br>
+            <input type="email" name="correo" id="correo" placeholder="Ingrese su correo electrónico"><br>
             <label for="">Contraseña</label><br>
-            <input type="password" name="pass" placeholder="Cree su contraseña"><br>
+            <input type="password" name="pass" id="pass" placeholder="Cree su contraseña"><br>
             <input type="submit" name="btn" value="Registrarse">
         </form>
     </div>
+
+    <script src="../../../js/formulario.js"></script>
+
     <footer>
         Todos los derechos reservados - ©Weeklyst 2020
     </footer>
--- bd/BD_Weeklyst.sql
+++ bd/BD_Weeklyst.sql
@@ -1,113 +1,58 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 25-03-2022 a las 08:22:16
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `weeklystbd`
---
 CREATE DATABASE IF NOT EXISTS `weeklystbd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `weeklystbd`;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `fichas`
---
+CREATE TABLE `contacto` (
+  `id_contacto` int(11) NOT NULL,
+  `correo` varchar(150) NOT NULL,
+  `num_tel` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `fichas` (
   `id_ficha` int(11) NOT NULL,
   `nombre_programa` varchar(200) NOT NULL,
   `num_ficha` varchar(50) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `fichas`
---
-
 INSERT INTO `fichas` (`id_ficha`, `nombre_programa`, `num_ficha`, `id_user`) VALUES
-(1, 'ADSI', '2141041', 1),
-(2, 'ADSI', '2141041', 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `lista`
---
+(7, 'Análisis y Desarrollo de Sistemas de Información', '222369', NULL),
+(9, 'Electricidad', '2405987', NULL);
 
 CREATE TABLE `lista` (
   `id_lista` int(11) NOT NULL,
-  `fecha` date NOT NULL,
+  `id_ficha` int(11) NOT NULL,
   `id_aprendiz` int(11) NOT NULL,
+  `fecha` date NOT NULL,
   `id_registro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `lista`
---
-
-INSERT INTO `lista` (`id_lista`, `fecha`, `id_aprendiz`, `id_registro`) VALUES
-(1, '2022-03-28', 2, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `registros`
---
 
 CREATE TABLE `registros` (
   `id_registros` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `registros`
---
-
 INSERT INTO `registros` (`id_registros`, `descripcion`) VALUES
 (1, 'Asiste'),
 (2, 'Falla'),
 (3, 'Excusa');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `rol`
---
 
 CREATE TABLE `rol` (
   `id_user` int(11) NOT NULL,
   `descripcion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `rol`
---
-
 INSERT INTO `rol` (`id_user`, `descripcion`) VALUES
 (1, 'Instructor'),
 (2, 'Aprendiz');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
 
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
@@ -120,107 +65,62 @@ CREATE TABLE `usuario` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `usuario`
---
-
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `tipo_docu`, `num_docu`, `id_rol`, `correo`, `password`) VALUES
-(1, 'Mario', 'Marín', 'C.C.', '1587496326', 1, 'mariom15@misena.edu.co', 'ositosexy123'),
-(2, 'Valerie', 'Puentes', 'C.C.', '1036587402', 2, 'valerip55@misena.edu.co', '123456789');
+(6, 'Pepito', 'Perez', 'Cédula de Ciudadanía.', '1034654876', 2, 'pepito@misena.edu.co', 'asdasd');
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `fichas`
---
+ALTER TABLE `contacto`
+  ADD PRIMARY KEY (`id_contacto`);
+
 ALTER TABLE `fichas`
   ADD PRIMARY KEY (`id_ficha`),
   ADD KEY `id_user` (`id_user`);
 
---
--- Indices de la tabla `lista`
---
 ALTER TABLE `lista`
   ADD PRIMARY KEY (`id_lista`),
   ADD KEY `id_aprendiz` (`id_aprendiz`),
   ADD KEY `id_aprendiz_2` (`id_aprendiz`),
-  ADD KEY `id_registro` (`id_registro`);
+  ADD KEY `id_registro` (`id_registro`),
+  ADD KEY `id_ficha` (`id_ficha`);
 
---
--- Indices de la tabla `registros`
---
 ALTER TABLE `registros`
   ADD PRIMARY KEY (`id_registros`);
 
---
--- Indices de la tabla `rol`
---
 ALTER TABLE `rol`
   ADD PRIMARY KEY (`id_user`);
 
---
--- Indices de la tabla `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`),
   ADD KEY `id_rol` (`id_rol`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `fichas`
---
+ALTER TABLE `contacto`
+  MODIFY `id_contacto` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `fichas`
-  MODIFY `id_ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT de la tabla `lista`
---
 ALTER TABLE `lista`
-  MODIFY `id_lista` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_lista` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT de la tabla `registros`
---
 ALTER TABLE `registros`
   MODIFY `id_registros` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT de la tabla `rol`
---
 ALTER TABLE `rol`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `fichas`
---
 ALTER TABLE `fichas`
   ADD CONSTRAINT `fichas_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `lista`
---
 ALTER TABLE `lista`
   ADD CONSTRAINT `lista_ibfk_1` FOREIGN KEY (`id_registro`) REFERENCES `registros` (`id_registros`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `lista_ibfk_2` FOREIGN KEY (`id_aprendiz`) REFERENCES `usuario` (`id_usuario`);
+  ADD CONSTRAINT `lista_ibfk_2` FOREIGN KEY (`id_aprendiz`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lista_ibfk_3` FOREIGN KEY (`id_ficha`) REFERENCES `fichas` (`id_ficha`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `usuario`
---
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
